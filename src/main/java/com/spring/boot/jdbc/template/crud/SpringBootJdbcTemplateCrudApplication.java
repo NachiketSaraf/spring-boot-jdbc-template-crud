408fb5904ea093c8c885867970048b46b19fa0a8
NachiketSaraf <sarafnachiket@gmail.com>
2024-03-01 18:17:49 +0530

    Initial commit

---
+++ src/main/java/com/spring/boot/jdbc/template/crud/SpringBootJdbcTemplateCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.boot.jdbc.template.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcTemplateCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcTemplateCrudApplication.class, args);
+    }
+
+}
